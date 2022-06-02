0dea760e4703c4dd32dd6f1c330c76e2ac750538
SpiridonDumitru <73492328+SpiridonDumitru@users.noreply.github.com>
2022-06-02 12:24:37 +0300

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
 
-wget \
+	# GRAB THE ENTIRE WEBSITE:
+
+$ wget \
      --recursive \
      --no-clobber \
      --page-requisites \
@@ -9,4 +10,14 @@ wget \
      --restrict-file-names=windows \
      --domains website.org \
      --no-parent \
-	https://www.drshirley.org/greek/textbook02/contents.html
+        https://www.regular-expressions.info/completelines.html
+         
+#         The options are:
+#    --recursive: download the entire Web site.
+#    --domains website.org: don't follow links outside website.org.
+#    --no-parent: don't follow links outside the directory tutorials/html/.
+#    --page-requisites: get all the elements that compose the page (images, CSS and so on).
+#    --html-extension: save files with the .html extension.
+#    --convert-links: convert links so that they work locally, off-line.
+#    --restrict-file-names=windows: modify filenames so that they will work in Windows as well.
+#    --no-clobber: don't overwrite any existing files (used in case the download is interrupted and resumed).
