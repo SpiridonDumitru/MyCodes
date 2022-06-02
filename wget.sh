ec6117fa7924e420b5fbb710fa1de454e7bdc48e
SpiridonDumitru <73492328+SpiridonDumitru@users.noreply.github.com>
2022-06-02 12:20:13 +0300

    Add files via upload

---
+++ wget.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains website.org \
+     --no-parent \
+	https://www.drshirley.org/greek/textbook02/contents.html
